1de60042d6d129c1b369f190b16b03936ae92c23
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-01 18:12:57 +0000

    [Fix]: Betty style

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 /**
- *
- *
+ * binary_tree_node - creates a node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ * Return: A pointer to the new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
