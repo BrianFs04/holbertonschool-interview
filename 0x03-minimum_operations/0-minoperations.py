e57638387e00e7cab0f0685bc5e877a07d0b5d23
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-08 19:53:58 +0000

    [Add]: Condition

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -5,11 +5,13 @@
 def minOperations(n):
     """Calculates the fewest number of operations
     needed to result in exactly n H characters"""
-    if type(n) is not int:
+    if type(n) is not int or n <= 1:
         return 0
     res = 0
     for i in range(2, n):
         while(n % i == 0):
             res += i
             n /= i
+    if res is 0:
+        return(n)
     return(res)
