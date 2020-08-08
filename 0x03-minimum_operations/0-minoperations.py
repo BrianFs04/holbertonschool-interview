7c2351bf07fd78bb90e5c417644bcb23ae4c93d4
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-08 20:08:56 +0000

    [Fix]: Setting return as an int

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,7 +6,7 @@ def minOperations(n):
     """Calculates the fewest number of operations
     needed to result in exactly n H characters"""
     if type(n) is not int or n <= 1:
-        return 0
+        return(0)
     res = 0
     for i in range(2, n):
         while(n % i == 0):
@@ -14,4 +14,4 @@ def minOperations(n):
             n /= i
     if res is 0:
         return(n)
-    return(res)
+    return(int(res))
