e0d98ab91a1cd9137edb0f27a995620c94068b65
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-31 00:06:47 +0000

    [Feature]: Reads stdin line by line and computes metrics

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -12,19 +12,33 @@ if __name__ == "__main__":
     ordered = {}
     size = i = 0
     count = 1
+    lst = [200, 301, 400, 401, 403, 404, 405, 500]
     data = sys.stdin
     try:
         for line in data:
             i += 1
             item = line.split(" ")
-            st_code = int(item[-2])
-            size += int(item[-1])
-            if ordered.get(st_code):
-                ordered[st_code] += count
-            else:
-                ordered[st_code] = count
-            if i % 10 == 0:
-                print_res()
+            try:
+                st_code = int(item[-2])
+            except:
+                pass
+            try:
+                size += int(item[-1])
+            except:
+                pass
+            try:
+                if st_code in lst:
+                    if ordered.get(st_code):
+                        ordered[st_code] += count
+                    else:
+                        ordered[st_code] = count
+            except:
+                pass
+            try:
+                if i % 10 == 0:
+                    print_res()
+            except:
+                pass
     except KeyboardInterrupt:
         print_res()
         raise
