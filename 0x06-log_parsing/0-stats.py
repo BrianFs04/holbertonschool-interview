3cc43f290a3663e599ff97d8c61d8c3c278ddf99
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-09-02 02:00:58 +0000

    [Feature]: Reads stdin line by line and computes metrics

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,4 +1,4 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 """Script that reads stdin line by line and computes metrics"""
 import sys
 
@@ -29,4 +29,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print_res()
         raise
-
