6853598904da813935543d1bab06be1e1551cf41
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-09-02 01:57:47 +0000

    [Feature]: Reads stdin line by line and computes metrics

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,32 @@
+ #!/usr/bin/python3
+"""Script that reads stdin line by line and computes metrics"""
+import sys
+
+
+def print_res():
+    print("File size: {}".format(size))
+    for k, v in sorted(ordered.items()):
+        print("{}: {}".format(k, v))
+
+if __name__ == "__main__":
+    ordered = {}
+    size = i = 0
+    count = 1
+    data = sys.stdin
+    try:
+        for line in data:
+            i += 1
+            item = line.split(" ")
+            st_code = item[-2]
+            size += int(item[-1])
+            if ordered.get(st_code):
+                ordered[st_code] += count
+            else:
+                ordered[st_code] = count
+            if i % 10 == 0:
+                print_res()
+        print_res()
+    except KeyboardInterrupt:
+        print_res()
+        raise
+
