9315aeee3393b142435bebf67c5c45387ace7857
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-09-03 17:45:42 +0000

    [Testing]: Edge cases

---
+++ 0x06-log_parsing/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+sys.stdout.write("{}\n".format("68.249.9.20 - [2017-02-05 23:31:22.452556] 'GET /projects/260 HTTP/1.1' 200 711"))
+sys.stdout.write("{}\n".format("Hello"))
+sys.stdout.write("{}\n".format("99.196.100.39 - [2017-02-05 23:31:22.954433] 'GET /projects/260 HTTP/1.1' 401 658"))
+sys.stdout.write("{}\n".format("128.230.61.246 - [2017-02-05 23:31:23.258076] 'GET /projects/260 HTTP/1.1' Hello 292"))
+sys.stdout.write("{}\n".format("116.82.223.35 - [2017-02-05 23:31:24.112360] 'GET /projects/260 HTTP/1.1' 301 842"))
+sys.stdout.write("{}\n".format("Holberton - [2017-02-05 23:31:25.003550] 'GET /projects/260 HTTP/1.1' 400 12"))
+sys.stdout.write("{}\n".format("7.179.133.121 - [2017-02-05 23:31:25.003550] 'GET /projects/260 HTTP/1.1' 400 12"))
+sys.stdout.write("{}\n".format("188.213.11.218-[2017-02-05 23:31:21.690755] 'GET /projects/260 HTTP/1.1' 401 1000"))
+sys.stdout.write("{}\n".format("128.230.61.246 - [2017-02-05 23:31:23.258076] 'GET /projects/260 HTTP/1.1' 301 292"))
+sys.stdout.flush()
