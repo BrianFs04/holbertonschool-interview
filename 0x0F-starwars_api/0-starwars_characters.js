b78e7650ab95ac00c365ce2267ca1b6ec2b38038
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-04-07 15:07:30 -0500

    [Feature]: Star Wars Characters

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+const request = require('request');
+
+const options = {
+  url: `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`,
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+getData();
+
+function getData () {
+  request(options, async (err, response, body) => {
+    if (!err) {
+      console.log(JSON.parse(body));
+      for (const people of JSON.parse(body).characters) {
+        await getCharactersNames(people);
+      }
+    } else {
+      console.log(err);
+    }
+  });
+}
+
+function getCharactersNames (people) {
+  return new Promise((resolve, reject) => {
+    request(people, (err, response, body) => {
+      if (!err) {
+        console.log(JSON.parse(body).name);
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
