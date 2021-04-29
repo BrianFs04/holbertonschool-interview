0c241bc19c1678ef56f2ed9c3d3bd0aa85e6d83e
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-04-29 18:23:13 -0500

    [Feature]: Using tabs

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -10,31 +10,31 @@
  */
 int search(int *array, int l, int r, int value)
 {
-    int i, mid;
+	int i, mid;
 
-    mid = l + (r - l) / 2;
-    if (l == r)
-    {
-        if (value < array[l] || value > array[r])
-        {
-            printf("Searching in array: %d\n", array[mid]);
-            return (-1);
-        }
-        return (l);
-    }
-    printf("Searching in array:");
-    for (i = l; i <= r; i++)
-    {
-        if (i != r)
-            printf(" %d,", array[i]);
-        else
-            printf(" %d\n", array[i]);
-    }
+	mid = l + (r - l) / 2;
+	if (l == r)
+	{
+		if (value < array[l] || value > array[r])
+		{
+			printf("Searching in array: %d\n", array[mid]);
+			return (-1);
+		}
+		return (l);
+	}
+	printf("Searching in array:");
+	for (i = l; i <= r; i++)
+	{
+		if (i != r)
+			printf(" %d,", array[i]);
+		else
+			printf(" %d\n", array[i]);
+	}
 
-    if (array[mid] < value)
-        return (search(array, mid + 1, r, value));
-    else
-        return (search(array, l, mid, value));
+	if (array[mid] < value)
+		return (search(array, mid + 1, r, value));
+	else
+		return (search(array, l, mid, value));
 }
 
 /**
@@ -46,7 +46,7 @@ int search(int *array, int l, int r, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-    if (!array)
-        return (-1);
-    return (search(array, 0, size - 1, value));
+	if (!array)
+		return (-1);
+	return (search(array, 0, size - 1, value));
 }
