e76dbd891c015f5a3c7c579196381692ec1c1f7d
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-06-10 21:06:11 -0500

    [Feature]: Header file

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -1,23 +1,7 @@
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef SORT_H
+#define SORT_H
 
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
+void merge_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
+#endif
